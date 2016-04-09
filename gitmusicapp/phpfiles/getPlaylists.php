082966c305ea5bb389987576924e5d481f6b247c
sabbakilam <pitdrop@gmail.com>
2016-04-09 18:40:49 +0000

    php destination fixed

--- gitmusicapp/phpfiles/getPlaylists.php
+++ gitmusicapp/phpfiles/getPlaylists.php
@@ -1,12 +1,12 @@
 <?php
 
 $root = $_SERVER['DOCUMENT_ROOT'] . "/";
-$filename = "lists.json";
+$filename = "gitmusicapp/lists.json";
 $destination = $root . $filename;
 $source = $_POST['lists'];
 
 file_put_contents($destination, $source);
 
-exit("Check for saved list");
+exit("Check for saved file.");
 
 ?>
