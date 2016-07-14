ea7e4ff056a4d9423217b42c5d8d0d586d42dc29
sabbakilam <pitdrop@gmail.com>
2016-07-14 03:22:02 +0000

    centered music app

--- apps/music/main.js
+++ apps/music/main.js
@@ -1,3 +1,25 @@
 document.body.onload = function(){
+    var titles = document.getElementsByClassName("title");
+    [].forEach.call(titles, function(m){
+        var top = 0.5*(window.innerHeight) + "px";
+        styles(m)
+            ("background","gray")
+            ("text-align","center")
+            ("border-radius","5px")
+            ("margin-top",top)
+            ("","")
+            ("","")
+            ("","")
+        ;
+    });
     document.getElementById("splashScreen").style.opacity = "0";
+    //------
+    function styles(object){
+        var f = function f(prop, value){
+            object.style[prop] = value;
+            return f;
+        };
+        return f;
+    } 
 };
+
--- apps/template/main.js
+++ apps/template/main.js
@@ -1,3 +1,4 @@
 document.body.onload = function(){
     document.getElementById("splashScreen").style.opacity = "0";
 };
+var layers
