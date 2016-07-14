af28d6ee80f06bbe14e20230a1f3472f571365d3
sabbakilam <pitdrop@gmail.com>
2016-07-14 03:31:55 +0000

    music app again

--- apps/music/main.js
+++ apps/music/main.js
@@ -1,13 +1,13 @@
 document.body.onload = function(){
     var titles = document.getElementsByClassName("title");
     [].forEach.call(titles, function(m){
-        var top = 0.5*(window.innerHeight) + "px";
+        var top = 0.5*(window.innerHeight) - 0.5*(m.getBoundingClientRect().height) + "px";
         styles(m)
             ("background","gray")
             ("text-align","center")
             ("border-radius","5px")
             ("margin-top",top)
-            ("","")
+            ("color","white")
             ("","")
             ("","")
         ;
