43b40fb28c97b1f9b57fddbe3c6bb2f95b1350a3
sabbakilam <pitdrop@gmail.com>
2016-08-03 22:45:08 +0000

    maybe all sliders work10

--- badslider/main.js
+++ badslider/main.js
@@ -1,6 +1,11 @@
 var $ = {
     realSliderMouseIsDown: false,
-    url: "https://SabbaKilam.github.io/music/"
+    url: "https://SabbaKilam.github.io/music/",
+    adjustRem: function adjustRem(){
+        var newRem = 5 + window.innerWidth/50;
+        document.documentElement.style.fontSize = newRem + "px";
+        return newRem;
+    }
 };
 $.id = function id(idString){
     return document.getElementById(idString);
@@ -9,41 +14,46 @@ $.attach = function attach(idString){
     //depends on $.id method
     $[idString] = $.id(idString);
 };
-$.attach("realSlider");
-$.attach("customSlider");
-$.attach("player");
-$.attach("songTitle");
-$.attach("customButton");
-$.player.src = $.url + "eyesface.mp3";
-$.songTitle.innerHTML = $.player.src;
-$.realSlider.onmousedown = function(e){
-    $.realSliderMouseIsDown = true;
-    var sizeInfo = e.target.getBoundingClientRect();
-    var left = sizeInfo.left;
-    var width = sizeInfo.width;
-    var distance = e.clientX - left;
-    var pct = distance /width;
-    $.player.volume = pct;
-    pct = parseInt(100 * pct, 10);
-    $.realSlider.value = pct;
+window.onload = function(){
+    $.adjustRem();
+    window.onresize = $.adjustRem;
+    $.attach("realSlider");
+    $.attach("customSlider");
+    $.attach("player");
+    $.attach("songTitle");
+    $.attach("customButton");
+    $.player.src = $.url + "eyesface.mp3";
+    $.songTitle.innerHTML = $.player.src;
+    $.realSlider.onmousedown = function(e){
+        $.realSliderMouseIsDown = true;
+        var sizeInfo = e.target.getBoundingClientRect();
+        var left = sizeInfo.left;
+        var width = sizeInfo.width;
+        var distance = e.clientX - left;
+        var pct = distance /width;
+        $.player.volume = pct;
+        pct = parseInt(100 * pct, 10);
+        $.realSlider.value = pct;
+        
+        var buttonWidth = $.customButton.getBoundingClientRect().width;
+        var halfWidth = buttonWidth/2;
+        $.customButton.style.left = $.leftFromPct($.customSlider, e.target.value/100) - halfWidth + "px";
+        $.player.volume = e.target.value /100;    
+    };
+    $.realSlider.onmouseup = function(e){
+        $.realSliderMouseIsDown = false;
+    };
+    $.realSlider.oninput  = function(e){
+        var buttonWidth = $.customButton.getBoundingClientRect().width;
+        var halfWidth = buttonWidth/2;
+        $.customButton.style.left = $.leftFromPct($.customSlider, e.target.value/100) - halfWidth + "px";
+        $.player.volume = e.target.value /100;    
+    };
+    $.leftFromPct = function(parent, pct){
+        var sizeInfo = parent.getBoundingClientRect();
+        var width = sizeInfo.width;
+        var left = width * pct;
+        return left;
+    };    
     
-    var buttonWidth = $.customButton.getBoundingClientRect().width;
-    var halfWidth = buttonWidth/2;
-    $.customButton.style.left = $.leftFromPct($.customSlider, e.target.value/100) - halfWidth + "px";
-    $.player.volume = e.target.value /100;    
-};
-$.realSlider.onmouseup = function(e){
-    $.realSliderMouseIsDown = false;
-};
-$.realSlider.oninput  = function(e){
-    var buttonWidth = $.customButton.getBoundingClientRect().width;
-    var halfWidth = buttonWidth/2;
-    $.customButton.style.left = $.leftFromPct($.customSlider, e.target.value/100) - halfWidth + "px";
-    $.player.volume = e.target.value /100;    
-};
-$.leftFromPct = function(parent, pct){
-    var sizeInfo = parent.getBoundingClientRect();
-    var width = sizeInfo.width;
-    var left = width * pct;
-    return left;
 };
