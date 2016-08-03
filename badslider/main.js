96de5c4d3968309ab52ccc3848bee34a0a4ead8e
sabbakilam <pitdrop@gmail.com>
2016-08-03 21:12:43 +0000

    maybe all sliders work-3

--- badslider/main.js
+++ badslider/main.js
@@ -1,5 +1,6 @@
 var $ = {
-    realSliderMouseIsDown: false
+    realSliderMouseIsDown: false,
+    url: "https://SabbaKilam.github.io/music/"
 };
 $.id = function id(idString){
     return document.getElementById(idString);
@@ -10,16 +11,30 @@ $.attach = function attach(idString){
 };
 $.attach("realSlider");
 $.attach("customSlider");
+$.attach("player");
+$.player.src = $.url + "eyesface.mp3";
+$.customSlider.innerHTML = $.player.src;
 $.realSlider.onmousedown = function(e){
     $.realSliderMouseIsDown = true;
+    var sizeInfo = e.target.getBoundingClientRect();
+    var left = sizeInfo.left;
+    var width = sizeInfo.width;
+    var distance = e.clientX - left;
+    var pct = distance /width;
+    pct = parseInt(100 * pct, 10);
+    $.realSlider.value = pct;
+    $.customSlider.innerHTML = pct + "%";    
 };
 $.realSlider.onmouseup = function(e){
     $.realSliderMouseIsDown = false;
 };
 $.realSlider.onmousemove = function(e){
-    if($.realSliderMouseIsDown){
-        var sizeInfo = e.target.getBoundingClientRect();
-        var left = sizeInfo.left;
+    var sizeInfo = e.target.getBoundingClientRect();
+    var left = sizeInfo.left;
+    var right = sizeInfo.right    
+    var leftIsGood = left <= e.clientX;
+    var rightIsGood = right >= e.clientX;
+    if($.realSliderMouseIsDown && leftIsGood && rightIsGood){;
         var width = sizeInfo.width;
         var distance = e.clientX - left;
         var pct = distance /width;
