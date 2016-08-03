34728a1533c18d009eaae4c33ceda7d5c3331dc8
sabbakilam <pitdrop@gmail.com>
2016-08-03 22:29:11 +0000

    maybe all sliders work-9

--- badslider/main.js
+++ badslider/main.js
@@ -26,35 +26,20 @@ $.realSlider.onmousedown = function(e){
     $.player.volume = pct;
     pct = parseInt(100 * pct, 10);
     $.realSlider.value = pct;
-    //$.customSlider.innerHTML = pct + "%";  
-    //$.customButton.style.left = e.target.value + "px";  
-    $.customButton.style.left = $.leftFromPct($.customSlider, e.target.value/100) + "px";
+    
+    var buttonWidth = $.customButton.getBoundingClientRect().width;
+    var halfWidth = buttonWidth/2;
+    $.customButton.style.left = $.leftFromPct($.customSlider, e.target.value/100) - halfWidth + "px";
     $.player.volume = e.target.value /100;    
 };
 $.realSlider.onmouseup = function(e){
     $.realSliderMouseIsDown = false;
 };
-/*
-$.realSlider.onmousemove = function(e){
-    var sizeInfo = e.target.getBoundingClientRect();
-    var left = sizeInfo.left;
-    var right = sizeInfo.right;   
-    var leftIsGood = left <= e.clientX;
-    var rightIsGood = right >= e.clientX;
-    if($.realSliderMouseIsDown && leftIsGood && rightIsGood){
-        var width = sizeInfo.width;
-        var distance = e.clientX - left;
-        var pct = distance /width;
-        pct = parseInt(100 * pct, 10);
-        $.realSlider.value = pct;
-        $.customSlider.innerHTML = pct + "%";
-    }
-};
-*/
 $.realSlider.oninput  = function(e){
-    //$.customSlider.innerHTML = e.target.value + "%";
-    $.customButton.style.left = $.leftFromPct($.customSlider, e.target.value/100) + "px";
-    $.player.volume = e.target.value /100;  
+    var buttonWidth = $.customButton.getBoundingClientRect().width;
+    var halfWidth = buttonWidth/2;
+    $.customButton.style.left = $.leftFromPct($.customSlider, e.target.value/100) - halfWidth + "px";
+    $.player.volume = e.target.value /100;    
 };
 $.leftFromPct = function(parent, pct){
     var sizeInfo = parent.getBoundingClientRect();
