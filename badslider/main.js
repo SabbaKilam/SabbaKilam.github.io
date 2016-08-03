6faae4ac5079ac8c7940cc44e9e35bb2b55d78ad
sabbakilam <pitdrop@gmail.com>
2016-08-03 20:53:28 +0000

    maybe all sliders work

--- badslider/main.js
+++ badslider/main.js
@@ -1,15 +1,30 @@
-var $ = {};
+var $ = {
+    realSliderMouseIsDown: false
+};
 $.id = function id(idString){
     return document.getElementById(idString);
 };
-$.realSlider = $.id("realSlider");
-$.realSlider.onclick = function(e){
-    var sizeInfo = e.target.getBoundingClientRect();
-    var left = sizeInfo.left;
-    var width = sizeInfo.width;
-    var distance = e.clientX - left;
-    var pct = distance /width;
-    pct = parseInt(100 * pct, 10);
-    $.realSlider.value = pct;
-    //alert(pct);
+$.attach = function attach(idString){
+    //depends on $.id method
+    $[idString] = $.id(idString);
+};
+$.attach("realSlider");
+$.attach("customSlider");
+$.realSlider.onmousedown = function(e){
+    $.realSliderMouseIsDown = true;
+};
+$.realSlider.onmouseup = function(e){
+    $.realSliderMouseIsDown = false;
+};
+$.realSlider.onmousemove = function(e){
+    if($.realSliderMouseIsDown){
+        var sizeInfo = e.target.getBoundingClientRect();
+        var left = sizeInfo.left;
+        var width = sizeInfo.width;
+        var distance = e.clientX - left;
+        var pct = distance /width;
+        pct = parseInt(100 * pct, 10);
+        $.realSlider.value = pct;
+        $.customSlider.innerHTML = pct + "%";
+    }
 };
