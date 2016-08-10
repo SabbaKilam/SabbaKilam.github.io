d70e0f4f47b515c147d0397fa277fe42697d9950
sabbakilam <pitdrop@gmail.com>
2016-08-10 19:36:49 +0000

    good slider diamond

---
+++ goodslider/main.js
@@ -0,0 +1,28 @@
+function id(idString){
+    return document.getElementById(idString);
+}
+
+//---------------------
+var faceSlider = id("faceSlider");
+var realSlider = id("realSlider");
+var btn = id("btn");
+//----------------------
+realSlider.oninput = moveBtn;
+window.onload = moveBtn;
+window.onresize = moveBtn;
+//----------------------
+function moveBtn(){
+    var buttonWidth = btn.getBoundingClientRect().width;
+    /*
+        Use the value of the realSlider
+        to calculate the left margin of the button
+        which is a percentage of the faceSlider's width
+        minus half the button's width
+    */
+    //get the faceSlider info
+    var faceInfo = faceSlider.getBoundingClientRect();
+    var sliderWidth = faceInfo.width;
+    var btnMarginLeft = realSlider.value/100 * sliderWidth  - buttonWidth/2 ;
+    
+    btn.style.marginLeft = btnMarginLeft + "px";
+}
