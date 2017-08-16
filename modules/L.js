dbc83a2730a380291ba7925db03620b0b0c18032
sabbakilam <pitdrop@gmail.com>
2017-08-15 21:45:32 -0400

    L plus styles

--- modules/L.js
+++ modules/L.js
@@ -1,9 +1,17 @@
-var L = {};
+//improved-library-abbas
+var L = {}
+L.styles = function(styleString){
+  let keyValue = styleString.split(':')
+  this.style[keyValue[0].trim()] = keyValue[1].trim()
+  return this.styles  
+}
 L.attachAllElementsById = function(here){
-    let allElements = document.getElementsByTagName('*');
-    [].forEach.call(allElements, function(element){
+    let allElements = document.getElementsByTagName('*')
+    let array = []
+    array.forEach.call(allElements, function(element)  {
         if(element.id){
-            here[element.id] = element;
+            here[element.id] = element
+            element.styles = L.styles.bind(element)          
         }
-    });
-};
+    })
+}
