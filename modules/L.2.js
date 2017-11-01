c6dad0ccdfdcb5f066ba1ab867ad037227af75f5
sabbakilam <pitdrop@gmail.com>
2017-11-01 09:46:46 -0400

    latest L.js

--- modules/L.2.js
+++ modules/L.2.js
@@ -1,15 +1,15 @@
 /*
   Author:  Abbas Abdulmalik
   Created: ~ May, 2017
-  Revised: October 31, 2017 
+  Revised: August 14, 2017 
   Original Filename: L.js 
   Purpose: a small personal re-usable js library for a simple MVC architecture
-  Notes: Now qualifyFunction helper doesn't return true for empty arrays (no vacuous truth)
+  Notes: 
 */
 
 var L = {}
 L.styles = function(styleString){
-  const colonPosition = styleString.indexOf(':')
+  const colonPosition = styleString.indexOf(':');
   const property = styleString.slice(0, colonPosition)
   const value = styleString.slice(colonPosition + 1)
   this.style[property] = value
@@ -17,27 +17,15 @@ L.styles = function(styleString){
   return this.styles  
 }
 
-L.attributes = function(attributeString){
-  const assignmentPosition = attributeString.indexOf('=')
-  const attribute = attributeString.slice(0, assignmentPosition)
-  const value = attributeString.slice(assignmentPosition + 1)
-  this.setAttribute(attribute, value)
-  
-  return this.attributes
-}
-
-L.attribs = L.attributes // a shorter reference
-
 L.attachAllElementsById = function(here){
-  let allElements = document.getElementsByTagName('*')
-  let array = []
-  array.forEach.call(allElements, function(element)  {
-      if(element.id){
-          here[element.id] = element
-          element.styles = L.styles.bind(element) // attach L's styles() method here
-          element.attributes = L.attributes.bind(element) // attach L's atributes() method here
-      }
-  })
+    let allElements = document.getElementsByTagName('*')
+    let array = []
+    array.forEach.call(allElements, function(element)  {
+        if(element.id){
+            here[element.id] = element
+            element.styles = L.styles.bind(element) // attach L's styles() method here         
+        }
+    })
 }
 
 L.noPinchZoom = function(){
@@ -57,15 +45,11 @@ L.runQualifiedMethods = function(functionQualifiers, object, runNextUpdate){
   
   //-----| helpers |-----//
   function qualifyFunction(functionName){
-    const isQualified = functionQualifiers[functionName].every( qualifier => qualifier) &&
-                        !!functionQualifiers.length
+    const isQualified = functionQualifiers[functionName].every( qualifier => qualifier)
     return isQualified
   }
   function runFunction(functionName){
-    if(typeof object[functionName] === 'function'){
-      object[functionName]()        
-    }
-   
+    object[functionName]()    
     /*
       If the prefix of this function's name is 'set' (for updating the MODEL),
       and there is a similarly named function with a prefix of 'show' (for updating the VIEW),
@@ -74,7 +58,7 @@ L.runQualifiedMethods = function(functionQualifiers, object, runNextUpdate){
     let prefix = functionName.slice(0,3)
     let newFunctionName = 'show' + functionName.slice(3)
     
-    if(prefix === 'set' && typeof object[newFunctionName] === 'function'){
+    if(prefix === 'set' && object[newFunctionName]){
       object[newFunctionName]()
     }
   }
